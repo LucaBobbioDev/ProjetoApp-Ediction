03180072c31579044c5cb437076ab31951c968c5
Luca Bobbio <bobbio.luca.117@gmail.com>
2022-10-11 17:43:42 -0300

    Routing Configuration

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Login } from "../screens/Login";
+import { Home } from "../screens/Home";
+import { Bookmarked } from "../screens/Bookmarked";
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Bookmarked" component={Bookmarked} />
+    </Stack.Navigator>
+  );
+}
