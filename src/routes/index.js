11993a1e547297e8eea24f03f66134acbafe6e4b
Luca Bobbio <bobbio.luca.117@gmail.com>
2022-10-15 14:22:50 -0300

    Nova rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Login  from "../screens/Login";
 import Home  from "../screens/Home";
+import Word from '../screens/Word';
 import Bookmarked  from "../screens/Bookmarked";
 
 const Stack = createStackNavigator();
@@ -9,8 +10,9 @@ const Stack = createStackNavigator();
 export default function Routes() {
   return (
     <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name='Word' component={Word}/>
       <Stack.Screen name="Bookmarked" component={Bookmarked} />
     </Stack.Navigator>
   );
